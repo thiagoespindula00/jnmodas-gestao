f4341209316020b5fadd8bd0c95221b508fdad20
thiagoespindula00 <thiagoespindula00@gmail.com>
2025-06-06 20:00:30 -0300

    Criado módulo de clientes

---
+++ cliente-ms/src/main/java/thiagoespindula00/jnmodasgestao/cliente/ClienteApplication.java
@@ -0,0 +1,11 @@
+package thiagoespindula00.jnmodasgestao.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteApplication.class, args);
+    }
+}
