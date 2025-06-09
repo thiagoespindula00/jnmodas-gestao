db0a4a936660d652413595f515376e938c9934eb
thiagoespindula00 <thiagoespindula00@gmail.com>
2025-06-09 12:02:31 -0300

    Criado módulo de fornecedores

---
+++ fornecedor-ms/src/main/java/thiagoespindula00/jnmodasgestao/fornecedor/FornecedorApplication.java
@@ -0,0 +1,11 @@
+package thiagoespindula00.jnmodasgestao.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FornecedorApplication.class, args);
+    }
+}
---
+++ fornecedor-ms/src/main/java/thiagoespindula00/jnmodasgestao/fornecedor/trata_erros/ValidacaoException.java
@@ -0,0 +1,7 @@
+package thiagoespindula00.jnmodasgestao.fornecedor.trata_erros;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
